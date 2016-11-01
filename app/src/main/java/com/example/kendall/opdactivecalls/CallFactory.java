baf3a8c77424fd4d5144aa6d0e0007d20a3aba57
Kendall Jackson <quailpecker@gmail.com>
2016-11-01 15:48:41 +0000

    Forgot CallFactory

---
+++ app/src/main/java/com/example/kendall/opdactivecalls/CallFactory.java
@@ -0,0 +1,18 @@
+package com.example.kendall.opdactivecalls;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kendall Jackson on 11/1/16.
+ */
+public class CallFactory {
+    public static ArrayList<Call> generateCallsWithXML(String xmlString){
+        ArrayList<Call> populatedList = new ArrayList<Call>();
+        for (int i=0; i<=6; ++i){
+            populatedList.add(stringBuilder);
+            //Might not be best way to populate, can't access var from protected method
+
+        }
+    }
+
+}
